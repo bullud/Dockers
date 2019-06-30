a8ae5e6fb2e751d603624be5e31f0830a52e3ec1
DianLi <dli@gpu038.aibee.cn>
2019-06-30 16:29:23 +0800

    modify docker name

--- run_cuda9_docker.sh
+++ run_cuda9_docker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+image=registry.aibee.cn/aibee/dli/10_3.6_1.1:0.0.1
+image=registry.aibee.cn/aibee/dli/9_27_041:0.0.1
 sudo nvidia-docker run --rm -v /mnt:/mnt -v /ssd:/ssd -p 9998:9999 -p 8021:22 -it registry.aibee.cn/aibee/dli/36_041:0.0.1 bash
