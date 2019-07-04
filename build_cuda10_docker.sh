096837f8beb444465e0770d29072db7bad27121c
DianLi <dli@gpu002.aibee.cn>
2019-07-04 14:44:44 +0800

    add new docker

--- build_cuda10_docker.sh
+++ build_cuda10_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker build --force-rm -t registry.aibee.cn/aibee/dli/3.6_1.1:0.0.1 -f Dockerfile.devel.cuda10.cudnn7.python36.pytorch11.ubuntu1604 .
+sudo docker build --force-rm -t registry.aibee.cn/aibee/dli/10_36_10:0.0.1 -f Dockerfile.devel.cuda10.cudnn7.python36.pytorch10.ubuntu1604 .
